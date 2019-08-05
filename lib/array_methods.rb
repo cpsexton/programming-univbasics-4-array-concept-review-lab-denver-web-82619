efd6349979cd5f8089302c8db7d5fb4b89819d64
Christopher Sexton <cpsexton@gmail.com>
2019-08-05 19:57:10 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -6,15 +6,15 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
- array.length.times { |array|
- max = array.max
-    }
-    p max
+# array.length.times { |array|
+# max = array.max
+#    }
+#    p max
 end
 
 def find_min_value(array)
- array.length.times { |array| 
- min = array.min
- puts min
- }
+# array.length.times { |array| 
+# min = array.min
+# puts min
+# }
 end
